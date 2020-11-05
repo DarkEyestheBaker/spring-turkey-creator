7b81b8b436a28449ac9c00584f932f3e9b2afa28
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-11-04 22:00:40 -0500

    Turkey Creator

---
+++ turkey-creator/src/main/java/com/turkey/business/package-info.java
@@ -0,0 +1,1 @@
+package com.turkey.business;
---
+++ turkey-creator/src/main/java/com/turkey/web/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author Julie
+ *
+ */
+package com.turkey.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+	
